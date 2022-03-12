bdc6fdb72af3b4234bfaef9195ac582e4afc8bfe
grind <mohamed.heni@insat.u-carthage.tn>
2022-03-12 23:14:20 +0100

    second commit

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ myBtn.addEventListener("click",()=>{
     var input=myInput.value;
     
         var myItem=document.createElement("li");
-        myItem.innerHTML= input + `<button class="done">done</button><button class="delete">delete</button>`;
+        myItem.innerHTML= input + `<div class="buttons"><button class="done">done</button><button class="delete">delete</button></div>`;
         myList.appendChild(myItem);
         var myDone=myItem.querySelector(".done");
         myDone.addEventListener("click",()=>{
-            myItem.style.textDecoration="line-through";
+            if (myItem.style.textDecoration!="line-through"){
+                myItem.style.textDecoration="line-through";
+            }else{
+                myItem.style.textDecoration="none"
+            }
         })
         var myDelete=myItem.querySelector(".delete");
         myDelete.addEventListener("click",()=>{
