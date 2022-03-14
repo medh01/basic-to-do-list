b56641feae85497fe70a8b748ce4b945b8a9e9e2
grind <mohamed.heni@insat.u-carthage.tn>
2022-03-14 14:09:17 +0100

    hello

--- script.js
+++ script.js
@@ -6,24 +6,24 @@ var myInput=document.querySelector("input");
 
 
 myBtn.addEventListener("click",()=>{
-    var input=myInput.value;
-    
+        
+        var input=myInput.value;
         var myItem=document.createElement("li");
-        myItem.innerHTML= input + `<div class="buttons"><button class="done">done</button><button class="delete">delete</button></div>`;
+        myItem.innerHTML= input;
         myList.appendChild(myItem);
-        var myDone=myItem.querySelector(".done");
-        myDone.addEventListener("click",()=>{
-            if (myItem.style.textDecoration!="line-through"){
-                myItem.style.textDecoration="line-through";
-            }else{
-                myItem.style.textDecoration="none"
-            }
-        })
-        var myDelete=myItem.querySelector(".delete");
-        myDelete.addEventListener("click",()=>{
-            myItem.remove();
-        })
+        myList.innerHTML+=`<i class="fa-solid fa-square-check done"></i><i class="fa-solid fa-trash delete"></i>`;
+        var myDone=document.querySelectorAll(".done");
+        var myDelete=document.querySelectorAll(".delete");
+        var listItem=document.querySelectorAll("li");
 
-
-
+        for(var i=0;i<listItem.length;i++){
+            myDone[i].addEventListener("click",()=>{
+                listItem[i].style.textDecoration="line-through";
+            })
+            myDelete[i].addEventListener("click",()=>{
+                listItem[i].remove();
+                myDone[i].remove();
+                myDelete[i].remove();  
+            })
+        }
 })
