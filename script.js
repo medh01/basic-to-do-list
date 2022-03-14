faf583a680e7d4d631ac200731c1b9c589dedbe8
grind <mohamed.heni@insat.u-carthage.tn>
2022-03-14 14:09:40 +0100

    commit

--- script.js
+++ script.js
@@ -4,26 +4,39 @@ var myBtn=document.querySelector(".input button");
 var myList=document.querySelector("ul");
 var myInput=document.querySelector("input");
 
+function f(){
+    var myInputValue=myInput.value;
+    if(myInputValue){
+        myInput.value="";
+        var myItem=document.createElement("li");
+        var myText=document.createElement("p");
+        var myDelete=document.createElement("i")
+        var myDone=document.createElement("i");
+        myText.innerText=myInputValue;
+        myDelete.setAttribute("class","fa-solid fa-trash delete");
+        myDone.setAttribute("class","fa-solid fa-square-check done");
+        myItem.appendChild(myText);
+        myItem.appendChild(myDone);
+        myItem.appendChild(myDelete);
+        myList.appendChild(myItem);
+     
+        myDone.addEventListener("click",()=>{
+            myText.style.textDecoration="line-through";
+        })
+     
+        myDelete.addEventListener("click",()=>{
+         myItem.remove();
+     })
+    }
+}
 
 myBtn.addEventListener("click",()=>{
-        
-        var input=myInput.value;
-        var myItem=document.createElement("li");
-        myItem.innerHTML= input;
-        myList.appendChild(myItem);
-        myList.innerHTML+=`<i class="fa-solid fa-square-check done"></i><i class="fa-solid fa-trash delete"></i>`;
-        var myDone=document.querySelectorAll(".done");
-        var myDelete=document.querySelectorAll(".delete");
-        var listItem=document.querySelectorAll("li");
+  f();
 
-        for(var i=0;i<listItem.length;i++){
-            myDone[i].addEventListener("click",()=>{
-                listItem[i].style.textDecoration="line-through";
-            })
-            myDelete[i].addEventListener("click",()=>{
-                listItem[i].remove();
-                myDone[i].remove();
-                myDelete[i].remove();  
-            })
-        }
+})
+
+myInput.addEventListener("keydown",(e)=>{
+    if(e.keyCode===13){
+        f();
+    }
 })
